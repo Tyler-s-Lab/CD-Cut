c52c530c6de83d1fb4e81d4451d52bc76dce7181
Tyler True <TylerTrueZ@outlook.com>
2026-08-12 21:19:24 +0800

    Use CUE

--- CD-Cut/Program.cs
+++ CD-Cut/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace CD_Cut {
 
@@ -33,85 +32,87 @@ namespace CD_Cut {
 			CueFile cue = new();
 			cue.ReadFromFile(cuePath);
 
-			Cuts(Path.GetDirectoryName(cuePath) ?? "", cue);
+			string dir = Path.GetDirectoryName(cuePath) ?? "";
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
 
-		}
+			for (int i = 0, n = cue.Tracks.Count; i < n; ++i) {
+				Track t = cue.Tracks[i];
 
-		public static void Cuts(string dir, CueFile cue) {
-			string source = Path.IsPathFullyQualified(data.File) ? data.File : Path.Combine(dir, data.File);
-
-			TimeSpan nextOffset = TimeSpan.Zero;
-			if (data.AudioTracks.Count > 0) {
-				data.AudioTracks[0].Offsets.Sort((x, y) => x.Index - y.Index);
-				nextOffset = ;
-			}
-
-			data.AudioTracks.Sort((x, y) => x.Index - y.Index);
-			for (int i = 0; i < data.AudioTracks.Count; i++) {
-				TimeSpan start = nextOffset;
-				TimeSpan end = TimeSpan.MaxValue;
-				if (i + 1 < data.AudioTracks.Count) {
-					data.AudioTracks[i + 1].Offsets.Sort((x, y) => x.Index - y.Index);
-					nextOffset = ComputeOffset(data.AudioTracks[i + 1].Offsets.Select(x => x.Time).ToArray());
-					end = nextOffset;
+				string source = Path.Combine(dir, t.filepath);
+				if (!File.Exists(source)) {
+					source = Path.Combine(dir, Path.GetFileNameWithoutExtension(cuePath) + ".flac");
 				}
-				Cut(source, Path.Combine(dir, $"Cuts"), start, end, data.AudioTracks[i], data);
+				if (!File.Exists(source)) {
+					source = Path.Combine(dir, Path.GetFileNameWithoutExtension(cuePath) + ".wav");
+				}
+				if (!File.Exists(source)) {
+					source = Path.Combine(dir, Path.GetFileNameWithoutExtension(cuePath) + ".mp3");
+				}
+				if (!File.Exists(source)) {
+					continue;
+				}
+
+				TimeSpan ss = TimeSpan.Zero, to = TimeSpan.MaxValue;
+				if (t.offset01 is double start)
+					ss = TimeSpan.FromMilliseconds(start);
+				if (i + 1 < n)
+					if (cue.Tracks[i + 1].offset00 is double end)
+						to = TimeSpan.FromMilliseconds(end);
+					else if (cue.Tracks[i + 1].offset01 is double end2)
+						to = TimeSpan.FromMilliseconds(end2);
+
+				t.filepath = source;
+				Console.WriteLine($"{t.index}: {t.offset01} ({t.filepath})");
+				Cut(t, ss, to);
 			}
 		}
 
-		public static void Cut(string srcPath, string dstDir, TimeSpan start, TimeSpan end, TrackData data, AlbumData albumData) {
-			if (!Directory.Exists(dstDir))
-				Directory.CreateDirectory(dstDir);
-
+		public static void Cut(Track t, TimeSpan ss, TimeSpan to) {
 			string filename;
-			filename = $"{data.Title}.flac";
+			filename = $"{t.index}-{t.metadata.Title}.flac";
 			filename = CleanNameForPath(filename);
-			string targetPath = Path.Combine(dstDir, filename);
+			string targetPath = filename;
 
-			string cmdStart = $"-loglevel warning -y -i \"{srcPath}\"";
-			string cmdCut = $"-ss {start.Hours}:{start.Minutes}:{start.Seconds}.{start.Milliseconds}";
-			if (end < TimeSpan.MaxValue) {
-				cmdCut += $" -to {end.Hours}:{end.Minutes}:{end.Seconds}.{end.Milliseconds}";
-			}
-			string cmdOut = $"-c:a flac";
-			string cmdMeta = "";
-			if (!(string.IsNullOrEmpty(data.Title) || string.IsNullOrWhiteSpace(data.Title)))
-				cmdMeta += $" -metadata TITLE=\"{data.Title}\"";
-			if (!(string.IsNullOrEmpty(data.Performer) || string.IsNullOrWhiteSpace(data.Performer)))
-				cmdMeta += $" -metadata AUTHOR=\"{data.Performer}\"";
-			if (!(string.IsNullOrEmpty(albumData.Performer) || string.IsNullOrWhiteSpace(albumData.Performer)))
-				cmdMeta += $" -metadata ALBUM_ARTIST=\"{albumData.Performer}\"";
-			if (!(string.IsNullOrEmpty(albumData.Title) || string.IsNullOrWhiteSpace(albumData.Title)))
-				cmdMeta += $" -metadata ALBUM=\"{albumData.Title}\"";
-			if (!(string.IsNullOrEmpty(data.Composer) || string.IsNullOrWhiteSpace(data.Composer)))
-				cmdMeta += $" -metadata COMPOSER=\"{data.Composer}\"";
-			if (!(string.IsNullOrEmpty(albumData.Year) || string.IsNullOrWhiteSpace(albumData.Year)))
-				cmdMeta += $" -metadata YEAR=\"{albumData.Year}\"";
-			cmdMeta += $" -metadata TRACK=\"{data.Index}\"";
-			if (!(string.IsNullOrEmpty(albumData.Genre) || string.IsNullOrWhiteSpace(albumData.Genre)))
-				cmdMeta += $" -metadata GENRE=\"{albumData.Genre}\"";
+			List<string> arglist = [];
+			arglist.AddRange([
+				"-loglevel", "warning",
+				"-y",
+				"-i", t.filepath,
+				"-ss", $"{ss.Hours}:{ss.Minutes}:{ss.Seconds}.{ss.Milliseconds}"
+			]);
+			if (to < TimeSpan.MaxValue)
+				arglist.AddRange([
+					"-to", $"{to.Hours}:{to.Minutes}:{to.Seconds}.{to.Milliseconds}"
+				]);
+			arglist.AddRange([
+				"-metadata", $"TRACK={t.index}"
+			]);
 
-			ProcessStartInfo processStartInfo = new() {
-				FileName = "ffmpeg",
-				Arguments = $"{cmdStart} {cmdCut} {cmdOut} {cmdMeta} \"{targetPath}\""
-			};
+			if (!string.IsNullOrEmpty(t.metadata.Title))
+				arglist.AddRange(["-metadata", $"TITLE={t.metadata.Title}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Performer))
+				arglist.AddRange(["-metadata", $"AUTHOR={t.metadata.Performer}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Performer))
+				arglist.AddRange(["-metadata", $"ALBUM_ARTIST={t.metadata.Performer}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Title))
+				arglist.AddRange(["-metadata", $"ALBUM={t.metadata.Album}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Composer))
+				arglist.AddRange(["-metadata", $"COMPOSER={t.metadata.Composer}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Year))
+				arglist.AddRange(["-metadata", $"YEAR={t.metadata.Year}"]);
+			if (!string.IsNullOrEmpty(t.metadata.Genre))
+				arglist.AddRange(["-metadata", $"GENRE={t.metadata.Genre}"]);
 
-			Console.WriteLine();
-			Console.WriteLine(processStartInfo.Arguments);
-			Console.WriteLine();
+			arglist.AddRange([
+				"-c:a", "flac",
+				targetPath
+			]);
 
-			using Process process = Process.Start(processStartInfo);
-			process.WaitForExit();
-		}
+			ProcessStartInfo processStartInfo = new("ffmpeg.exe", arglist);
 
-		public static TimeSpan ComputeOffset(TimeSpan[] timeSpans) {
-			if (timeSpans.Length == 0) {
-				throw new ArgumentException("TimeSpans too short.");
-			}
-			if (timeSpans.Length == 1) {
-				return timeSpans[0];
-			}
-			return (timeSpans[0] + timeSpans[1]) / 2;
+			using var process = Process.Start(processStartInfo);
+			process?.WaitForExit();
 		}
 
 		private static string CleanNameForPath(string name) {
